db31e365b89d04ae8582fccb9ccb791cf12f2272
Koech-code <nixon.koech@moringaschool.com>
2021-08-16 22:18:49 +0300

    created forms to enable users to interact with our application

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,10 +4,18 @@ from wtforms import SelectField, TextAreaField, StringField, SubmitField
 from wtforms.validators import Required
 
 class postForm(FlaskForm):
-    title = StringField()
-    post =TextAreaField()
-    category =SelectField()
-    submit =SubmitField()
+    title = StringField('Title', validators=[Required()])
+    post =TextAreaField('Pitch', validators=[Required()])
+    category =SelectField('Category', choices=[('PRODUCT', 'PRODUCT'), ('IDEA', 'IDEA'), ('BUSINESS', 'BUSINESS')], validators=[Required()])
+    submit =SubmitField('post')
+
+class commentForm(FlaskForm):
+    title =StringField('Title', validators=[Required()])
+    comment =TextAreaField('Comment', validators=[Required()])
+    submit =SubmitField('post')
+
+class vote(FlaskForm):
+    submit =SubmitField('like')
 
 
 
