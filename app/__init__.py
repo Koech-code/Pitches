0731ab252aef56b30cdbd28a227a3dd798f12de5
Koech-code <nixon.koech@moringaschool.com>
2021-08-16 18:17:59 +0300

    registered the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,10 @@ app=Flask(__name__)
 def create_app(config_name):
     app.config.from_object(config_options[config_name])
     bootstrap.init_app(app)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     
+
     return app
 
 
